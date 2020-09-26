e75e04856dfaf1909b00d52f999343bd94c27bcb
wesley34 <wesleyyam10@gmail.com>
2020-09-26 10:50:16 +0800

    .

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -17,23 +17,45 @@ def evaluate_salads():
     logging.info("My result :{}".format(result))
     return json.dumps(result);
 
+def get_cheapest_queue(cheapest_at_the_store,current_price):
+    
+    for i in reversed(range(len(cheapest_at_the_store))):
+        if cheapest_at_the_store[i] > current_price:
+            cheapest_at_the_store[i] = current_price
+            break
+    cheapest_at_the_store.sort()
+  
+    return cheapest_at_the_store
+
+
+
 def solution(number_of_salads,salad_prices_street_map):
     min_money = 99999999999
-    for stores in salad_prices_street_map:
-        salad_counter = 0
-        money = 0
-        for salad in stores:
-            if salad != "X":
-                salad_counter+=1
-                money += int(salad)
-        if salad_counter >= number_of_salads:
-            if money < min_money:
-                min_money = money
-    if min_money == 99999999999:
-        min_money = 0
-    
-    return{ "result":min_money}
+    original = 99999999999
+    for store in salad_prices_street_map:
 
+        consec_counter = 0
+        one_store_total_price = 0
+        temp = []
+        for salad in store:
+            if salad != "X":
+                consec_counter+=1
+                current_price = int(salad)
+                temp.append(current_price)
+            
+            if salad == "X":
+                if consec_counter >= number_of_salads:
+                # sort it
+                    temp.sort()
+                    # get the answer
+                    for i in range(number_of_salads):
+                        one_store_total_price += temp[i]
+                    if min_money > one_store_total_price:
+                        min_money =one_store_total_price
+
+    if min_money == original:
+        min_money = 0
+    return{ "result":min_money}
 
 
 
