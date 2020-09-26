4d8f40a349e5b40be84c48f1c30e19427e5b99a7
wesley34 <wesleyyam10@gmail.com>
2020-09-26 13:36:47 +0800

    a

--- codeitsuisse/routes/inventory.py
+++ codeitsuisse/routes/inventory.py
@@ -35,76 +35,77 @@ def solution(searchItemName,searchResult):
     
 
 def search_snap(word_1,word_2):
-    word_1_counter = 0
-    word_2_counter = 0
-    copy = word_2
-    
-    word_1 = word_1.lower()
-    word_2 = word_2.lower()
-    
-    #print(word_1,word_2)
-    markup = []
-    
-    while word_2_counter < len(word_2) or word_1_counter < len(word_1):
-        if word_1_counter > len(word_1)-1:
-            break
-        if word_2_counter > len(word_2)-1:
-            break
-        #print(word_1[word_1_counter],word_2[word_2_counter])
-        if word_1[word_1_counter] == word_2[word_2_counter]:
-            word_1_counter += 1
-            word_2_counter += 1
-        else:
-            # find word_1 the nearest exsit temr in word_2
-            #print("Enter")
-            find_space = word_2.find(" ",word_2_counter)
-            found_pointer = word_2.find(word_1[word_1_counter],word_2_counter)
-            is_found_larger_than_space = find_space < found_pointer and find_space != -1
-            if is_found_larger_than_space:
-                # + that string to word_2
-                string_to_be_inserted = "+"+word_1[word_1_counter]
-                markup.append([word_2_counter+1, string_to_be_inserted])
-                word_1_counter+=1
-                continue
-            if found_pointer == -1 :
-                #print("Find for -1 case")
-                #print(word_1[word_1_counter],word_2[word_2_counter])
-                # check if need replacement
-                #print("you want to check")
-                #print(word_2[word_2_counter+1], word_1[word_1_counter+1])
-                if word_1_counter+1 >= len(word_1)-1:
-                    break
-                check_ch = word_1[word_1_counter+1]
-                
-                
-                    
-                after_pointer = word_2.find(check_ch,word_2_counter)
-                #print(word_1[word_1_counter],word_2[word_1_counter])
-                #print("Check",check_ch)
-                if check_ch == " ":
-                    #print("HI")
-                    after_pointer = -1
-                if after_pointer != -1:
-                    string_to_be_replaced = word_1[word_1_counter]
-                    markup.append([word_2_counter, string_to_be_replaced])
-                    word_2_counter+=1
-                    word_1_counter+=1
-                else: 
+    try:
+        word_1_counter = 0
+        word_2_counter = 0
+        copy = word_2
+        
+        word_1 = word_1.lower()
+        word_2 = word_2.lower()
+        
+        #print(word_1,word_2)
+        markup = []
+        
+        while word_2_counter < len(word_2) or word_1_counter < len(word_1):
+            if word_1_counter > len(word_1)-1:
+                break
+            if word_2_counter > len(word_2)-1:
+                break
+            #print(word_1[word_1_counter],word_2[word_2_counter])
+            if word_1[word_1_counter] == word_2[word_2_counter]:
+                word_1_counter += 1
+                word_2_counter += 1
+            else:
+                # find word_1 the nearest exsit temr in word_2
+                #print("Enter")
+                find_space = word_2.find(" ",word_2_counter)
+                found_pointer = word_2.find(word_1[word_1_counter],word_2_counter)
+                is_found_larger_than_space = find_space < found_pointer and find_space != -1
+                if is_found_larger_than_space:
                     # + that string to word_2
                     string_to_be_inserted = "+"+word_1[word_1_counter]
                     markup.append([word_2_counter+1, string_to_be_inserted])
                     word_1_counter+=1
-            else:   
-                # - the in-between string
-                for in_between in range(word_2_counter,found_pointer):
-                    markup.append([in_between, "-"+copy[in_between]])
-                word_2_counter = found_pointer
-             
-   
-    # print(markup)
-    answer = copy
+                    continue
+                if found_pointer == -1 :
+                    #print("Find for -1 case")
+                    #print(word_1[word_1_counter],word_2[word_2_counter])
+                    # check if need replacement
+                    #print("you want to check")
+                    #print(word_2[word_2_counter+1], word_1[word_1_counter+1])
+                    if word_1_counter+1 >= len(word_1)-1:
+                        break
+                    check_ch = word_1[word_1_counter+1]
+                    
+                    
+                        
+                    after_pointer = word_2.find(check_ch,word_2_counter)
+                    #print(word_1[word_1_counter],word_2[word_1_counter])
+                    #print("Check",check_ch)
+                    if check_ch == " ":
+                        #print("HI")
+                        after_pointer = -1
+                    if after_pointer != -1:
+                        string_to_be_replaced = word_1[word_1_counter]
+                        markup.append([word_2_counter, string_to_be_replaced])
+                        word_2_counter+=1
+                        word_1_counter+=1
+                    else: 
+                        # + that string to word_2
+                        string_to_be_inserted = "+"+word_1[word_1_counter]
+                        markup.append([word_2_counter+1, string_to_be_inserted])
+                        word_1_counter+=1
+                else:   
+                    # - the in-between string
+                    for in_between in range(word_2_counter,found_pointer):
+                        markup.append([in_between, "-"+copy[in_between]])
+                    word_2_counter = found_pointer
+                
     
-    try:
+        # print(markup)
+        answer = copy
+        
+    
         if len(markup) > 0:
             question_list = [i for i in copy]
             print(markup)
@@ -113,12 +114,12 @@ def search_snap(word_1,word_2):
                 #print(i[0],i[1])
                 question_list[i[0]] = i[1]
                 answer = "".join(str(x) for x in question_list)
-    
 
-            answer = "".join(str(x) for x in question_list)
-        
-        return answer
+
+                
+            
+            return answer
     except:
-        return answer
+        return word_2
 
 
